1640f2a2eab2989b0ac7efaba6efa9349922b1db
ActuaryEmma <emmanyakio@gmail.com>
2024-06-05 21:45:17 +0300

    Login Page with PIN Keypad and user info fields

--- app/src/main/java/com/example/qrpesa/pages/LoginPage.kt
+++ app/src/main/java/com/example/qrpesa/pages/LoginPage.kt
@@ -1,2 +1,128 @@
 package com.example.qrpesa.pages
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun QRPesaLogin() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = Color.Black
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.height(100.dp))
+            Text(
+                text = "SH",
+                style = MaterialTheme.typography.h5,
+                color = Color.White
+            )
+            Text(
+                text = "Sharon Wendoh",
+                style = MaterialTheme.typography.body1,
+                color = Color.White
+            )
+            Text(
+                text = "0702020101",
+                style = MaterialTheme.typography.body1,
+                color = Color.White
+            )
+            Spacer(modifier = Modifier.height(80.dp))
+
+            Text(
+                text = "Enter Mpesa Pin",
+                style = MaterialTheme.typography.body1,
+                color = Color.White
+            )
+
+            Row(
+                modifier = Modifier.padding(top = 16.dp),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "1")
+                }
+                Spacer(modifier = Modifier.width(8.dp))
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "2")
+                }
+                Spacer(modifier = Modifier.width(8.dp))
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "3")
+                }
+            }
+            Row(
+                modifier = Modifier.padding(top = 8.dp),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "4")
+                }
+                Spacer(modifier = Modifier.width(8.dp))
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "5")
+                }
+                Spacer(modifier = Modifier.width(8.dp))
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "6")
+                }
+            }
+            Row(
+                modifier = Modifier.padding(top = 8.dp),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "7")
+                }
+                Spacer(modifier = Modifier.width(8.dp))
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "8")
+                }
+                Spacer(modifier = Modifier.width(8.dp))
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "9")
+                }
+            }
+            Row(
+                modifier = Modifier.padding(top = 8.dp),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Cancel")
+                }
+                Spacer(modifier = Modifier.width(8.dp))
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "0")
+                }
+                Spacer(modifier = Modifier.width(8.dp))
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Enter")
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewQRPesaLogin() {
+    QRPesaLogin()
+}
